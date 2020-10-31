b93024db235a825da2f7cdc22e5413810a278f35
Jonathan Emil Mogensen <jmog@itu.dk>
2020-10-31 19:20:48 +0100

    Extended MoveToPositionAction
    Extended the action script, so it can now support 2 to n positions, twoway movement, cycling movement, and the inspector only shows the fields and booleans that are relevant

--- Assets/_MY_ASSETS/Scripts/Actions_Interaction/MoveToPositionAction.cs
+++ Assets/_MY_ASSETS/Scripts/Actions_Interaction/MoveToPositionAction.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -7,15 +8,17 @@ public enum State_PrototypeMoveAction { DEACTIVATED, ACTIVATED, FINISHED }
 
 public class MoveToPositionAction : InteractableAction
 {
-    private State_PrototypeMoveAction state = State_PrototypeMoveAction.DEACTIVATED;
-    [SerializeField] private Transform startPosition;
-    [SerializeField] private Transform endPosition;
-    [SerializeField] private float moveSpeed;
+    public State_PrototypeMoveAction state = State_PrototypeMoveAction.DEACTIVATED;
     private Transform target;
     private Transform last;
     private float moveFraction = 0f;
     private float positionWaitTimeCounter;
+    private int moveToIndex = 1;
+    private bool forward = true;
+    [SerializeField] private float moveSpeed;
+    [SerializeField, Tooltip("There must always be atleast two elements in the list for the script to behave properly.")] private List<Transform> movePositions;
     [SerializeField, HideInInspector] private bool twoWay;
+    [SerializeField, HideInInspector] private bool cycle;
     [SerializeField, HideInInspector] private float positionWaitTime = 1f;
 
 
@@ -32,11 +35,11 @@ public class MoveToPositionAction : InteractableAction
     void Start()
     {
         positionWaitTimeCounter = positionWaitTime;
-        transform.position = startPosition.position;
+        transform.position = movePositions[0].position;
         //startPosition = transform.TransformPoint(startPosition);
         //endPosition = transform.TransformPoint(endPosition);
-        last = startPosition;
-        target = endPosition;
+        last = movePositions[0];
+        target = movePositions[moveToIndex];
 
     }
 
@@ -45,7 +48,8 @@ public class MoveToPositionAction : InteractableAction
     {
         if (state == State_PrototypeMoveAction.ACTIVATED)
         {
-            if (twoWay) TwoWayMovement();
+            if (cycle) CycleMovement();
+            else if (twoWay) TwoWayMovement();
             else OneWayMovement();
         }
     }
@@ -55,7 +59,7 @@ public class MoveToPositionAction : InteractableAction
         if (moveFraction < 1)
         {
             moveFraction += Time.deltaTime * moveSpeed;
-            transform.position = Vector3.Lerp(startPosition.position, endPosition.position, moveFraction);
+            transform.position = Vector3.Lerp(movePositions[moveToIndex-1].position, movePositions[moveToIndex].position, moveFraction);
         }
         else if (moveFraction >= 1) state = State_PrototypeMoveAction.FINISHED;
         //transform.position = Vector3.Lerp(transform.position, target, moveSpeed * Time.deltaTime);
@@ -64,7 +68,62 @@ public class MoveToPositionAction : InteractableAction
     {
         if (moveFraction < 1)
         {
-            moveFraction += Time.deltaTime * moveSpeed;
+            if (forward) 
+            {
+                last = movePositions[moveToIndex - 1];
+                target = movePositions[moveToIndex];
+            }
+            if (!forward)
+            {
+                last = movePositions[moveToIndex + 1];
+                target = movePositions[moveToIndex];
+            }
+            moveFraction += Time.deltaTime * moveSpeed / (last.position - target.position).magnitude;
+            transform.position = Vector3.Lerp(last.position, target.position, moveFraction);
+
+
+        }
+        else if (moveFraction >= 1)
+        {
+            if (positionWaitTimeCounter > 0f)
+            {
+                positionWaitTimeCounter -= Time.deltaTime;
+            }
+            else if (positionWaitTimeCounter <= 0f)
+            {
+                if (forward)
+                {
+                    if (moveToIndex == movePositions.Count - 1)
+                    {
+                        forward = false;
+                        moveToIndex--;
+                    }
+                    else moveToIndex++;
+                }
+                else if (!forward)
+                {
+                    if (moveToIndex == 0)
+                    {
+                        forward = true;
+                        moveToIndex++;
+                    }
+                    else moveToIndex--;
+                }
+                positionWaitTimeCounter = positionWaitTime;
+                moveFraction = 0f;
+            }
+        }
+    }
+
+    private void CycleMovement()
+    {
+        if (moveFraction < 1)
+        {
+            int lastIndex = moveToIndex-1;
+            if (moveToIndex == 0) lastIndex = movePositions.Count - 1; 
+            last = movePositions[lastIndex];
+            target = movePositions[moveToIndex];
+            moveFraction += Time.deltaTime * moveSpeed / (last.position - target.position).magnitude;
             transform.position = Vector3.Lerp(last.position, target.position, moveFraction);
         }
         else if (moveFraction >= 1)
@@ -75,14 +134,11 @@ public class MoveToPositionAction : InteractableAction
             }
             else if (positionWaitTimeCounter <= 0f)
             {
+                moveToIndex = (moveToIndex + 1) % (movePositions.Count);
                 positionWaitTimeCounter = positionWaitTime;
                 moveFraction = 0f;
-                var temp = last;
-                last = target;
-                target = temp;
             }
         }
-
     }
 
     [CustomEditor(typeof(MoveToPositionAction))]
@@ -93,9 +149,28 @@ public class MoveToPositionAction : InteractableAction
             base.OnInspectorGUI();
             var action = target as MoveToPositionAction;
 
-            action.twoWay = EditorGUILayout.Toggle("Two Way", action.twoWay, new GUILayoutOption[0]);
+            if (action.movePositions.Count == 2 && !action.twoWay)
+            {
+                action.twoWay = EditorGUILayout.Toggle(new GUIContent("Two Way"), action.twoWay, new GUILayoutOption[0]);
+            }
 
-            if (action.twoWay) action.positionWaitTime = EditorGUILayout.FloatField("Position Wait Time", action.positionWaitTime);
+            else if (action.movePositions.Count > 2 && !action.twoWay && !action.cycle)
+            {
+                action.twoWay = EditorGUILayout.Toggle(new GUIContent("Two Way"), action.twoWay, new GUILayoutOption[0]);
+                action.cycle = EditorGUILayout.Toggle(new GUIContent("Cycle", "The object will move in the following pattern: 0->1->2->...->n->0. And the object will repeat this pattern."), action.cycle, new GUILayoutOption[0]);
+            }
+
+            else if (action.twoWay && !action.cycle)
+            {
+                action.twoWay = EditorGUILayout.Toggle(new GUIContent("Two Way"), action.twoWay, new GUILayoutOption[0]);
+                action.positionWaitTime = EditorGUILayout.FloatField("Position Wait Time", action.positionWaitTime);
+            }
+
+            else if (action.movePositions.Count > 2 && !action.twoWay && action.cycle)
+            {
+                action.positionWaitTime = EditorGUILayout.FloatField("Position Wait Time", action.positionWaitTime);
+                action.cycle = EditorGUILayout.Toggle(new GUIContent("Cycle", "The object will move in the following pattern: 0->1->2->...->n->0. And the object will repeat this pattern."), action.cycle, new GUILayoutOption[0]);
+            }
 
         }
     }
