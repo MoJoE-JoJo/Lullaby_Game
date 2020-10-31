36c0e03a648de02dacf074a40572e1b0d8820516
Fernando Rodriguez Alvarez <fero@itu.dk>
2020-10-31 18:16:23 +0100

    Player can now move and jump

--- Assets/_MY_ASSETS/PlayerControls.cs
+++ Assets/_MY_ASSETS/PlayerControls.cs
@@ -78,6 +78,52 @@ public class @PlayerControls : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": false
                 }
             ]
+        },
+        {
+            ""name"": ""Player"",
+            ""id"": ""141f1024-2864-4acb-9c4d-0ff8ace6cb07"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""2b8cea39-9e99-42b8-82e7-d466ad2f5b69"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""ecbea8fe-64c8-495a-b322-afdf10cbfd4b"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""c7ceebe8-5df4-4b2c-a5a8-0f304288d687"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""fcce6fa0-70b8-4fd0-bdaf-fe5ed06f6df3"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
         }
     ],
     ""controlSchemes"": []
@@ -87,6 +133,10 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         m_NoteSelector_Move = m_NoteSelector.FindAction("Move", throwIfNotFound: true);
         m_NoteSelector_Sing = m_NoteSelector.FindAction("Sing", throwIfNotFound: true);
         m_NoteSelector_LockNote = m_NoteSelector.FindAction("LockNote", throwIfNotFound: true);
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -181,10 +231,56 @@ public class @PlayerControls : IInputActionCollection, IDisposable
         }
     }
     public NoteSelectorActions @NoteSelector => new NoteSelectorActions(this);
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Jump;
+    public struct PlayerActions
+    {
+        private @PlayerControls m_Wrapper;
+        public PlayerActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
     public interface INoteSelectorActions
     {
         void OnMove(InputAction.CallbackContext context);
         void OnSing(InputAction.CallbackContext context);
         void OnLockNote(InputAction.CallbackContext context);
+    }
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
     }
 }
--- Assets/_MY_ASSETS/Scripts/Player/PlayerController.cs
+++ Assets/_MY_ASSETS/Scripts/Player/PlayerController.cs
@@ -11,10 +11,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private RegisterActivators activators;
 
+    private Rigidbody2D _rb2d;
+
     [SerializeField]
     private SongData _songBeingSung;
     [SerializeField]
     private bool _isSinging;
+
+    public float playerSpeed;
+    public float jumpForce;
+    private bool _jumpFlag;
+    private bool _isGrounded;
+    
+    private Vector2 _move;
 
     private void Awake()
     {
@@ -22,19 +31,32 @@ public class PlayerController : MonoBehaviour
 
         _controls.NoteSelector.Move.performed += context => ActivateNoteSelector();
         _controls.NoteSelector.Move.canceled += context => DeactivateNoteSelector();
+
+        _controls.Player.Move.performed += context => _move = context.ReadValue<Vector2>();
+        _controls.Player.Move.canceled += context => _move = Vector2.zero;
+
+        _controls.Player.Jump.performed += context => Jump();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        _rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (noteSelector.activeSelf)
+        MovePlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_jumpFlag && _isGrounded)
         {
-            
+            _rb2d.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+            _jumpFlag = false;
+            _isGrounded = false;
         }
     }
 
@@ -73,5 +95,34 @@ public class PlayerController : MonoBehaviour
     {
         get => _isSinging;
         set => _isSinging = value;
+    }
+
+    void MovePlayer()
+    {
+        Vector2 move = new Vector2(_move.x, 0) * (playerSpeed * Time.deltaTime);
+        transform.Translate(move, Space.World);
+    }
+
+    void Jump()
+    {
+        if (_isGrounded)
+        {
+            _jumpFlag = true;
+        }
+    }
+
+    /*private void OnCollisionEnter2D(Collision2D other)
+    {
+        _isGrounded = true;
+    }*/
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        _isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        _isGrounded = false;
     }
 }
