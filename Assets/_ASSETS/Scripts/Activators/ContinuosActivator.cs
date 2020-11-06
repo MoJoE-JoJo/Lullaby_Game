442bb22dc39f3a435dbf1842d82a507e30447445
Jonathan Emil Mogensen <jmog@itu.dk>
2020-11-06 13:33:17 +0100

    Started work on ContinuosActivator

---
+++ Assets/_ASSETS/Scripts/Activators/ContinuosActivator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State_ContinuosActivator { IDLE, ACTIVATE, RECEIVING, DEACTIVATE }
+public class ContinuosActivator : Activator
+{
+    public override void ShowHint()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SongInput(SongData data)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    
+}
