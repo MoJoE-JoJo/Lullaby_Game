0dc7e00d9e72a0c84744b425649a9a2c0a6f9694
Jonathan Emil Mogensen <jmog@itu.dk>
2020-11-28 18:24:53 +0100

    New end fading

--- Assets/_ASSETS/Scripts/FinalDoor/HittingEnd.cs
+++ Assets/_ASSETS/Scripts/FinalDoor/HittingEnd.cs
@@ -6,24 +6,61 @@ using UnityEngine.SceneManagement;
 
 public class HittingEnd : MonoBehaviour
 {
-    [SerializeField] private Light2D doorLight;
-    [SerializeField] private float newIntensity;
+    //[SerializeField] private Light2D doorLight;
+    [SerializeField] private GameObject whiteOverlay;
+    private SpriteRenderer fadeSprite;
+    //[SerializeField] private float newIntensity;
     [SerializeField] private float fadeTime;
-    private float originalIntensity;
+    //private float originalIntensity;
     private float fadeTimer = 0.0f;
     private PlayerController pc;
     private bool end = false;
+    [SerializeField] private bool endCredits = false;
     // Start is called before the first frame update
     void Start()
     {
         pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-        originalIntensity = doorLight.intensity;
+        fadeSprite = whiteOverlay.GetComponent<SpriteRenderer>();
+        Color tmp = fadeSprite.color;
+        tmp.a = 0;
+        fadeSprite.color = tmp;
+        //originalIntensity = doorLight.intensity;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (end)
+        {
+            whiteOverlay.transform.position = pc.transform.position;
+            if(fadeTimer >= fadeTime)
+            {
+                SceneManager.LoadScene("End");
+            }
+            fadeTimer += Time.deltaTime;
+
+            Color tmp = fadeSprite.color;
+            tmp.a = fadeTimer / fadeTime;
+            fadeSprite.color = tmp;
+        }
+        else if (endCredits)
+        {
+
+            whiteOverlay.transform.position = pc.transform.position;
+            if (fadeTimer >= fadeTime)
+            {
+                gameObject.SetActive(false);
+            }
+            fadeTimer += Time.deltaTime;
+
+            // fade to solid color
+            Color tmp = fadeSprite.color;
+            tmp.a = 1.0f - (fadeTimer / fadeTime);
+            fadeSprite.color = tmp;
+        }
+
+        /*
+                 if (end)
         {
             if(fadeTimer >= fadeTime)
             {
@@ -34,6 +71,7 @@ public class HittingEnd : MonoBehaviour
             doorLight.intensity = Mathf.SmoothStep(originalIntensity, newIntensity, fraction);
             //doorLight.intensity += intensityIncrease * Time.deltaTime;
         }
+         */
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
