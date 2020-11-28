b9224d030aab521d0287038ff56620a62d6c5b06
Marcus <noller02@hotmail.com>
2020-11-28 17:28:28 +0100

    new elevator and bell stuff, included fiddling with loading chunks and stuff

---
+++ Assets/_ASSETS/Scripts/Interaction_Architecture/InteractableActions/SetActiveAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveAction : InteractableAction
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject thing;
+    [SerializeField] private bool onActivationSetActive;
+    [SerializeField] private bool canDeactivate = false;
+
+    public override void Activate()
+    {
+        thing.SetActive(onActivationSetActive);
+    }
+
+    public override void Deactivate()
+    {
+       if (canDeactivate) thing.SetActive(!onActivationSetActive);
+    }
+
+    public override void InputData(SongData data)
+    {
+    }
+
+    public override void Reset()
+    {
+        thing.SetActive(!onActivationSetActive);
+    }
+}
