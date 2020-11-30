2e6a44f6c6e056b867e08027da0a34884595343e
MartinfraDD <36191600+MartinfraDD@users.noreply.github.com>
2020-11-30 20:30:06 +0100

    Wrapping up

--- Assets/_ASSETS/Scripts/Text/FadeSpriteScript.cs
+++ Assets/_ASSETS/Scripts/Text/FadeSpriteScript.cs
@@ -7,46 +7,91 @@ public class FadeSpriteScript : MonoBehaviour
 {
     SpriteRenderer rend;
     public bool hasFaded;
-    public bool pressed;
-    public float fadeSpeed = .2f;
+    public bool pressedFadeIn;
+    public bool pressedFadeout;
+    public bool isInside;
+    public bool isOutside;
+    public float fadeInSpeed = .02f;
+    public float fadeOutSpeed = .02f;
     private void Start()
     {
         rend = GetComponent<SpriteRenderer>();
         Color c = rend.material.color;
-        c.a = 0f;
-        rend.material.color = c;
         
+        //rend.material.color = c;
+        rend.material.color = new Color(1, 1, 1, 0);
+        //StartCoroutine("FadeOut");
     }
 
     private void Update()
     {
-        if (pressed == true)
-        {
-            StartFading();
-            pressed = false;
-        }
+ 
     }
 
     IEnumerator FadeIn()
     {
-        for (float f = 0.0f; f <= 1 + 1; f += fadeSpeed)
+        if (isInside) { 
+        for (float f = 0.0f; f <= 1 + 1; f += fadeInSpeed)
         {
             Color c = rend.material.color;
             c.a = f;
             rend.material.color = c;
             yield return new WaitForSeconds(0.05f);
-        }
-    }
-
-    public void StartFading()
-    {
-        if (!hasFaded)
-        {
-            StartCoroutine("FadeIn");
             hasFaded = true;
         }
-
-
+            rend.material.color = new Color(1, 1, 1, 1);
+            yield return null;
+        }
+        else {
+            rend.material.color = new Color(1, 1, 1, 0);
+        }
     }
+
+
+    IEnumerator FadeOut()
+    {
+        if (isOutside)
+        {
+            for (float g = 1.0f; g >= -0.05f; g -= fadeOutSpeed)
+        {
+            Color d = rend.material.color;
+            d.a = g;
+            rend.material.color = d;
+            yield return new WaitForSeconds(0.05f);
+            hasFaded = false;
+        }
+            rend.material.color = new Color(1, 1, 1, 0);
+            yield return null;
+        }
+    }
+
+       public void StartFading()
+       {
+        isInside = true;
+        isOutside = false;
+        if (!hasFaded)
+           {
+               StartCoroutine("FadeIn");
+               
+           }
+       }
+
+       public void StartFadingOut()
+       {
+        isInside = false;
+        isOutside = true;
+           if (hasFaded)
+           {
+               StartCoroutine("FadeOut");
+               
+           }
+           else
+        {
+            rend.material.color = new Color(1, 1, 1, 0);
+        }
+       }
+    
 }
+
+
 
