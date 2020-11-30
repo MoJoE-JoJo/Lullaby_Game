943fb5cfc2b8501a2b5da058f043a51c23bc9b36
MartinfraDD <36191600+MartinfraDD@users.noreply.github.com>
2020-11-30 11:11:02 +0100

    Added text implimentation with shader

---
+++ Assets/_ASSETS/Scripts/Text/FadeSpriteScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeSpriteScript : MonoBehaviour
+{
+    SpriteRenderer rend;
+    public bool hasFaded;
+    public bool pressed;
+    public float timeToFinish;
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        Color c = rend.material.color;
+        c.a = 0f;
+        rend.material.color = c;
+        
+    }
+
+    private void Update()
+    {
+        if (pressed == true)
+        {
+            StartFading();
+            pressed = false;
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float f = 0.0f; f <= 1 + 1; f += 0.05f)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    public void StartFading()
+    {
+        if (!hasFaded)
+        {
+            StartCoroutine("FadeIn");
+            hasFaded = true;
+        }
+
+
+    }
+}
+
