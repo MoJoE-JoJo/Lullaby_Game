5c5314577beddec24419ff936982fd29d000a5e6
Jonathan Emil Mogensen <jmog@itu.dk>
2020-12-01 23:17:51 +0100

    Fixed weird, cannot play again bug
    It was that the end-credits, quit to menu button didn't unpause

---
+++ Assets/_ASSETS/Scripts/DisableGameObjectOnEnter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableGameObjectOnEnter : MonoBehaviour
+{
+    [SerializeField] private GameObject go;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            go.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
