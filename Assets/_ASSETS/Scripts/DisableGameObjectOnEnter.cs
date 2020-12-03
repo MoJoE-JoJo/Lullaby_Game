ec89f7df3d392a055f23ea3493f2eb7db6afbde7
Jonathan Emil Mogensen <jmog@itu.dk>
2020-12-03 11:39:29 +0100

    Tweaked elevator sound duration

--- Assets/_ASSETS/Scripts/DisableGameObjectOnEnter.cs
+++ Assets/_ASSETS/Scripts/DisableGameObjectOnEnter.cs
@@ -8,7 +8,7 @@ public class DisableGameObjectOnEnter : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.name.Equals("Moving Part"))
         {
             go.SetActive(false);
             gameObject.SetActive(false);
